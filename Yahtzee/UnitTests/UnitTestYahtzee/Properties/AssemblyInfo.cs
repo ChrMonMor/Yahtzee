76c6779e6c90be47c3444b1f8d0a67240714fb47
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-01-18 15:05:49 +0100

    Made the first fill and unittest-folder, but nothing has been coded yet in the program, that will all come tomorrow

---
+++ Yahtzee/UnitTests/UnitTestYahtzee/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestYahtzee")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestYahtzee")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5dd7277a-7138-4b15-b735-22a26373306e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
