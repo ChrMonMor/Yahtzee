81722907ff08e2cc52b3be2dbed3baa1ef77403b
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-01-20 15:10:28 +0100

    Got all the logic inside to play a game of yathzee. Now I will start doing a lot of unit test and clean up the code, so that it is easier for others to nanvigate. I migth add some other graffical elements, so that it is easier to read what is happing in the controller. I am also planing on expanding the types of Yathzee you can play in this one, as well have i ideas for bettering the board

--- Yahtzee/UnitTests/UnitTestYahtzee/UnitTestYathzee.cs
+++ Yahtzee/UnitTests/UnitTestYahtzee/UnitTestYathzee.cs
@@ -8,59 +8,262 @@ namespace UnitTestYahtzee
     public class UnitTestYathzee
     {
         [TestMethod]
-        public void TestMethodDiceOfAKindLowerSectionCalculatorTwoOfAkind()
+        public void TestMethodDiceOfKindsLowerSectionCalculatorTwoOfAkind()
         {
             //-- Arrange
             int[] array = { 6,6,2,2,3};
             int twoOfAKind = 2;
+            int pairs = 1;
             int expected = 12;
 
             //-- Act
-            var actaul = Program.DiceOfAKindLowerSectionCalculator(array, twoOfAKind);
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, twoOfAKind, pairs, false);
 
             //-- Assert
             Assert.AreEqual(expected, actaul);
         }
         [TestMethod]
-        public void TestMethodDiceOfAKindLowerSectionCalculatorThreeOfAkind()
+        public void TestMethodDiceOfKindsLowerSectionCalculatorThreeOfAkind()
         {
             //-- Arrange
             int[] array = { 6, 2, 2, 2, 3 };
             int threeOfAKind = 3;
+            int pairs = 1;
             int expected = 6;
 
             //-- Act
-            var actaul = Program.DiceOfAKindLowerSectionCalculator(array, threeOfAKind);
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, threeOfAKind, pairs, false);
 
             //-- Assert
             Assert.AreEqual(expected, actaul);
         }
 
         [TestMethod]
-        public void TestMethodDiceOfAKindLowerSectionCalculatorFourOfAkind()
+        public void TestMethodDiceOfKindsLowerSectionCalculatorFourOfAkind()
         {
             //-- Arrange
             int[] array = { 6, 3, 3, 3, 3 };
             int fourOfAKind = 4;
+            int pairs = 1;
             int expected = 12;
 
             //-- Act
-            var actaul = Program.DiceOfAKindLowerSectionCalculator(array, fourOfAKind);
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, fourOfAKind, pairs, false);
 
             //-- Assert
             Assert.AreEqual(expected, actaul);
         }
 
         [TestMethod]
-        public void TestMethodDiceOfAKindLowerSectionCalculatorFiveOfAkind()
+        public void TestMethodDiceOfKindsLowerSectionCalculatorFiveOfAkind()
         {
             //-- Arrange
             int[] array = { 2, 2, 2, 2, 2 };
             int fiveOfAKind = 5;
+            int pairs = 1;
             int expected = 10;
 
             //-- Act
-            var actaul = Program.DiceOfAKindLowerSectionCalculator(array, fiveOfAKind);
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, fiveOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorTwoOfAkindAndTwoPairs()
+        {
+            //-- Arrange
+            int[] array = { 6, 6, 2, 2, 3 };
+            int twoOfAKind = 2;
+            int pairs = 2;
+            int expected = 16;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, twoOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorTwoOfAkindAndTwoPairsButThereisYathzee()
+        {
+            //-- Arrange
+            int[] array = { 6, 6, 6, 6, 6 };
+            int twoOfAKind = 2;
+            int pairs = 2;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, twoOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorThreeOfAkindFalse()
+        {
+            //-- Arrange
+            int[] array = { 6, 6, 2, 2, 3 };
+            int twoOfAKind = 3;
+            int pairs = 1;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, twoOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorTwoOfAkindFalse()
+        {
+            //-- Arrange
+            int[] array = { 6, 1, 2, 4, 3 };
+            int twoOfAKind = 2;
+            int pairs = 1;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, twoOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorFourOfAkindFalse()
+        {
+            //-- Arrange
+            int[] array = { 6, 3, 5, 3, 3 };
+            int fourOfAKind = 4;
+            int pairs = 1;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, fourOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorFiveOfAkindFalse()
+        {
+            //-- Arrange
+            int[] array = { 2, 1, 2, 2, 2 };
+            int fiveOfAKind = 5;
+            int pairs = 1;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, fiveOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorTwoOfAkindAndTwoPairsFalse()
+        {
+            //-- Arrange
+            int[] array = { 6, 4, 2, 2, 3 };
+            int twoOfAKind = 2;
+            int pairs = 2;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, twoOfAKind, pairs, false);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+
+
+        [TestMethod]
+        public void TestMethodDiceOfKindsLowerSectionCalculatorFullHouse()
+        {
+            //-- Arrange
+            int[] array = { 4, 4, 2, 2, 4 };
+            int top = 3;
+            int maxjumps = 2;
+            int expected = 16;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, top, maxjumps, true);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodPyramidDiceLowerSectionCalculatorFullHouseFalseYathzee()
+        {
+            //-- Arrange
+            int[] array = { 1, 1, 1, 1, 1 };
+            int top = 3;
+            int maxjumps = 2;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, top, maxjumps, true);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodPyramidDiceLowerSectionCalculatorFullHouseFalseNonSimilar()
+        {
+            //-- Arrange
+            int[] array = { 1, 2, 3, 4, 5 };
+            int top = 3;
+            int maxjumps = 2;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceOfKindsLowerSectionCalculator(array, top, maxjumps, true);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceInARowLowerSectionCalculatorSmall()
+        {
+            //-- Arrange
+            int[] array = { 1, 2, 3, 4, 5 };
+            int smallest = 1;
+            int biggest = 5;
+            int expected = 15;
+
+            //-- Act
+            var actaul = Program.DiceInARowLowerSectionCalculator(array, smallest, biggest);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceInARowLowerSectionCalculatorSmallFalse()
+        {
+            //-- Arrange
+            int[] array = { 1, 2, 3, 4, 6 };
+            int smallest = 1;
+            int maxjumps = 5;
+            int expected = 0;
+
+            //-- Act
+            var actaul = Program.DiceInARowLowerSectionCalculator(array, smallest, maxjumps);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void TestMethodDiceInARowLowerSectionCalculatorRoyal()
+        {
+            //-- Arrange
+            int[] array = { 1, 2, 3, 4, 5, 6};
+            int smallest = 1;
+            int biggest = 6;
+            int expected = 30;
+
+            //-- Act
+            var actaul = Program.DiceInARowLowerSectionCalculator(array, smallest, biggest);
 
             //-- Assert
             Assert.AreEqual(expected, actaul);
--- Yahtzee/Yahtzee/Program.cs
+++ Yahtzee/Yahtzee/Program.cs
@@ -10,64 +10,69 @@ namespace Yahtzee
     {
         public static void Main(string[] args)
         {
-            //-- TK
+            //-- TK (gonna make this into a game setting constructor and give it differnt values, depending on the game you are playing)
+            string[] typeOfGame = NormalYahtzee();
             int numberOfPlayers = 1;
             int numberOfDiceRolls = 3;
             int numberOfDices = 5;
-            //-- TK^^
-            string[,] gameBoard = GameBoard(numberOfPlayers);
-            while (true)
-            {
-                Console.Clear();
-                GameBoardPrinter(gameBoard);
-                string playerCommand = "";
-                //-- Loop for the rounds 
-                for (int round = 1; round < gameBoard.Length; round++)
-                {
-                    //-- the loop for a players turn, so we know whoes turn it is
-                    for (int turn = 1; turn < numberOfPlayers; turn++)
-                    {
-                        //-- might change this to be a standalone method instead of a part of AddResultToGameboardFields, so that Sum is sperate and and skips the the two turns of "sum" and "bonus"
-                        if (gameBoard[round, 0] == "Sum")
-                        {
-                            gameBoard = AddUpperSectionTogether(gameBoard, turn);
-                            continue;
-                        }
-                        else if (gameBoard[round, 0] == "bonus")
-                        {
-                            gameBoard = CheckIfSumIsGoodForBonus(gameBoard, turn);
-                            continue;
-                        }
+            int bonus = 50;
 
-                        int[] playersDices = DiceRolledArray(numberOfDices);
-                        //-- loop for the number for rolls the player has
-                        for (int timesRolled = 0; timesRolled < numberOfDiceRolls; timesRolled++)
+            //-- TK^^
+
+
+            string[,] gameBoard = GameBoard(numberOfPlayers, typeOfGame);
+
+
+            Console.Clear();
+            GameBoardPrinter(gameBoard);
+            string playerCommand = "";
+            //-- Loop for the rounds 
+            for (int round = 1; round < typeOfGame.Length; round++)
+            {
+                //-- the loop for a players turn, so we know whoes turn it is
+                for (int turn = 1; turn <= numberOfPlayers; turn++)
+                {
+                    int[] playersDices = DiceRolledArray(numberOfDices);
+                    //-- might change this to be a standalone method instead of a part of AddResultToGameboardFields, so that Sum is sperate  and skips the two rounds of "sum" and "bonus"
+                    if (gameBoard[round, 0] == "Sum")
+                    {
+                        gameBoard[round, turn] = AddUpperSectionTogether(gameBoard, turn).ToString();
+                        gameBoard[round + 1, turn] = CheckIfSumIsGoodForBonus(gameBoard, numberOfDices, bonus).ToString();
+                        continue;
+                    }
+                    //-- might need to be change since it doesn't do anything but skip the players being in "bonus"-round
+                    if (gameBoard[round, 0] == "Bonus")
+                    {
+                        continue;
+                    }
+                    if (gameBoard[round, 0] == "Total")
+                    {
+                        gameBoard[round, turn] = PlayersTotalScore(gameBoard, turn, round).ToString();
+                        continue;
+                    }
+
+                    //-- loop for the number for rolls the player has
+                    for (int timesRolled = 0; timesRolled < numberOfDiceRolls; timesRolled++)
+                    {
+                        Console.Clear();
+                        GameBoardPrinter(gameBoard);
+                        playersDices = playersDices.OrderBy(i => i).ToArray();
+                        DiceArrayPrinter(playersDices);
+                        //-- 
+                        if (playerCommand == "stop" || timesRolled >= numberOfDiceRolls - 1)
                         {
-                            Console.Clear();
-                            GameBoardPrinter(gameBoard);
-                            playersDices = playersDices.OrderBy(i => i).ToArray();
-                            DiceArrayPrinter(playersDices);
-                            playerCommand = Console.ReadLine();
-                            //-- 
-                            if (playerCommand == "stop" || timesRolled >= 2)
-                            {
-                                gameBoard = AddResultToGameboardFields(playersDices, gameBoard, round, turn);
-                            }
-                            playersDices = DiceReRoller(playersDices, playerCommand);
+                            gameBoard = AddResultToGameboardFields(playersDices, gameBoard, round, turn);
+                            continue;
                         }
+                        playerCommand = Console.ReadLine();
+                        playersDices = DiceReRoller(playersDices, playerCommand);
                     }
                 }
             }
-        }
-
-        public static string[,] CheckIfSumIsGoodForBonus(string[,] gameBoard, int turn)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static string[,] AddUpperSectionTogether(string[,] gameBoard, int turn)
-        {
-            throw new NotImplementedException();
+            Console.Clear();
+            GameBoardPrinter(gameBoard);
+            Console.WriteLine("Game is Done");
+            Console.ReadLine();
         }
 
         //-- TK
@@ -75,59 +80,107 @@ namespace Yahtzee
         {
             switch (gameBoard[round,0])
             {
-                case "Ene'er":
+                case "Ene'ere":
                     gameBoard[round,turn] = SingleUpperSectionCalculator(playersDices, 1).ToString();
                     break;
-                case "To'er":
+                case "To'ere":
                     gameBoard[round, turn] = SingleUpperSectionCalculator(playersDices, 2).ToString();
                     break;
-                case "Tre'er":
+                case "Tre'ere":
                     gameBoard[round, turn] = SingleUpperSectionCalculator(playersDices, 3).ToString();
                     break;
-                case "Fire'er":
+                case "Fire'ere":
                     gameBoard[round, turn] = SingleUpperSectionCalculator(playersDices, 4).ToString();
                     break;
-                case "Fem'er":
+                case "Fem'ere":
                     gameBoard[round, turn] = SingleUpperSectionCalculator(playersDices, 5).ToString();
                     break;
-                case "Seks'er":
+                case "Seks'ere":
                     gameBoard[round, turn] = SingleUpperSectionCalculator(playersDices, 6).ToString();
                     break;
-                case "Sum":
-
-                    break;
                 case "Et Par":
-                    gameBoard[round, turn] = DiceOfAKindLowerSectionCalculator(playersDices, 2).ToString();
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 2, 1, false).ToString();
                     break;
                 case "To Par":
-
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 2, 2, false).ToString();
+                    break;
+                case "Tre Par":
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 2, 3, false).ToString();
                     break;
                 case "Tre enes":
-                    gameBoard[round, turn] = DiceOfAKindLowerSectionCalculator(playersDices, 3).ToString();
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 3, 1, false).ToString();
+                    break;
+                //-- Not too happy about this one, maybe there is a way to make this be a combo of both "Tre enes" & "To Par" 
+                case "To Par Tre enes":
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 3, 2, false).ToString();
                     break;
                 case "Fire enes":
-                    gameBoard[round, turn] = DiceOfAKindLowerSectionCalculator(playersDices, 4).ToString();
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 4, 1, false).ToString();
+                    break;
+                case "Fem enes":
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 5, 1, false).ToString();
                     break;
                 case "Den Lille":
-
+                    gameBoard[round, turn] = DiceInARowLowerSectionCalculator(playersDices, 1, 5).ToString();
                     break;
                 case "Den Store":
-
+                    gameBoard[round, turn] = DiceInARowLowerSectionCalculator(playersDices, 2, 5).ToString();
+                    break;
+                case "Cameron":
+                    gameBoard[round, turn] = DiceInARowLowerSectionCalculator(playersDices, 1, 6).ToString();
                     break;
                 case "Fuld Hus":
-
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, 5, 1, true).ToString();
                     break;
                 case "Changen":
                     gameBoard[round, turn] = playersDices.Sum().ToString();
                     break;
                 case "Yathzee":
-                    gameBoard[round, turn] = DiceOfAKindLowerSectionCalculator(playersDices, 5).ToString();
+                    gameBoard[round, turn] = DiceOfKindsLowerSectionCalculator(playersDices, playersDices.Length, 1, false).ToString();
                     break;
                 default:
                     break;
             }
             return gameBoard;
         }
+
+        public static int PlayersTotalScore(string[,] gameBoard, int turn, int round)
+        {
+            int score = 0;
+            //-- here we find out where we are starting the score-count from
+            for (int i = 0; i < round; i++)
+            {
+                if(gameBoard[i, 0] == "Sum")
+                {
+                    continue;
+                }
+                score += Convert.ToInt32(gameBoard[i, turn]);
+            }
+
+            return score;
+        }
+
+        //-- Counts up the dice the user has in a row, it starts looking for the "smallest" and ends if it got all the way up to the biggest and returns a value 
+        public static int DiceInARowLowerSectionCalculator(int[] playersDices, int smallest, int biggest)
+        {
+            int sum = 0;
+            if (biggest == 6)
+            {
+                sum = 9;
+            }
+            playersDices = playersDices.OrderBy(i => i).ToArray();
+            for (int i = 0; i < biggest; i++)
+            {
+                if(!Array.Exists(playersDices, x => x == smallest))
+                {
+                    return 0;
+                }
+                sum += smallest;
+                smallest++;
+            }
+            return sum;
+        }
+
         //-- TK^^
 
         //-- an Calculator for a single section in the Upper-Section
@@ -140,17 +193,68 @@ namespace Yahtzee
             }
             return sum;
         }
-        //-- an Calculator for dice of a kind in the Lower-Section
-        public static int DiceOfAKindLowerSectionCalculator(int[] playersDices, int howMany)
+        //-- gets the total sum of a players Upper-Section and returns a int
+        public static int AddUpperSectionTogether(string[,] gameBoard, int turn)
         {
-            int sum = 0;
+            //-- might change the hardcoded number here
+            int[] vs = new int[6];
+            for (int i = 0; i < vs.Length; i++)
+            {
+                vs[i] = Convert.ToInt32(gameBoard[i+1, turn]);
+            }
+            return vs.Sum();
+        }
+
+        //-- Checks if you have enought int the Upper Section to get the Bonus 
+        public static int CheckIfSumIsGoodForBonus(string[,] gameBoard, int numberOfDices, int bonus)
+        {
+            //-- find out what you minimum need ((number of dice * 3) / 4)
+            int vs = (numberOfDices * 3) / 4;
+            int needed = 0;
+            //-- this gets the sum needed to get the bonus
+            for (int i = 1;i < 7; i++)
+            {
+                needed += vs * i;
+            }
+            //-- finds the player who has just gotten their Upper Section sum calculated 
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                if(gameBoard[i,0] == "Sum")
+                {
+                    for (int j = 0; j < gameBoard.Length; j++)
+                    {
+                        if (gameBoard[i, j] != "" && gameBoard[i+1, j] == null)
+                        {
+                            vs = Convert.ToInt32(gameBoard[i, j]);
+                            if (vs < needed)
+                            {
+                                bonus = 0;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            return bonus;
+        }
+
+        //-- an Calculator for dice of a kind in the Lower-Section
+        public static int DiceOfKindsLowerSectionCalculator(int[] playersDices, int similar, int pairs, bool house)
+        {
+            int[] sum = new int[pairs];
             playersDices = playersDices.OrderBy(i => i).ToArray();
-            for (int i = playersDices.Length-1; i >= howMany-1; i--)
+            for (int i = playersDices.Length-1; i >= similar - 1; i--)
             {
                 int count = 0;
-                for (int j = 0; j < howMany; j++)
+                for (int j = 0; j < similar; j++)
                 {
-                    if(playersDices[i-j] != playersDices[i])
+                    //-- if the playersDices[i-j] is out of scope, than this breaks the loop
+                    if (i - j < 0)
+                    {
+                        break;
+                    }
+                    else if (playersDices[i-j] != playersDices[i])
                     {
                         break;
                     }
@@ -159,13 +263,36 @@ namespace Yahtzee
                         count++;
                     }
                 }
-                if (count == howMany)
+                //-- checks if the count match and that the dice ISN'T already there
+                if (count == similar && !Array.Exists(sum, x => x == playersDices[i]))
                 {
-                    sum = howMany * playersDices[i];
-                    break;
+                    sum[pairs-1] = playersDices[i];
+                    pairs--;
+                    if (house)
+                    {
+                        similar -= 1;
+                    }
                 }
+                //-- if the roll has a legal result, it will return the higest value here
+                if (pairs == 0)
+                {
+                    if (house)
+                    {
+                        //-- I'm using pairs here because it is already zero
+                        foreach (int item in sum)
+                        {
+                            pairs += item * ++similar; 
+                        }
+                        return pairs;
+                    }
+                    else
+                    {
+                        return sum.Sum() * similar;
+                    }
+                }
+                i -= count-1;
             }
-            return sum;
+            return 0;
         }
 
         //-- Rolls one Dice only
@@ -208,11 +335,8 @@ namespace Yahtzee
         }
 
         //-- Makes the gameboard 
-        public static string[,] GameBoard(int numberOfPlayers)
+        public static string[,] GameBoard(int numberOfPlayers, string[] typeOfGame)
         {
-            //-- TK
-            string[] typeOfGame = NormalYahtzee();
-            //-- TK^^
             string[,] board = new string[typeOfGame.Length,numberOfPlayers+1];
             for (int i = 0; i < typeOfGame.Length; i++)
             {
@@ -227,12 +351,12 @@ namespace Yahtzee
             return new string[] 
             {
                 "Spiller:",
-                "Ene'er",
-                "To'er",
-                "Tre'er",
-                "Fire'er",
-                "Fem'er",
-                "Seks'er",
+                "Ene'ere",
+                "To'ere",
+                "Tre'ere",
+                "Fire'ere",
+                "Fem'ere",
+                "Seks'ere",
                 "Sum",
                 "Bonus",
                 "Et Par",
